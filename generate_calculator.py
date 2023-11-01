d96d28f62847190245b0a12215d7435bc59e6831
Christoph Bless <github@cbless.de>
2023-11-01 11:26:58 +0100

    added weighing for consequences to risk calculator.

--- generate_calculator.py
+++ generate_calculator.py
@@ -2,15 +2,40 @@
 
 
 """
+Installing requirements:
+
+pip install -r requirements.txt
+
+or manual with:
+
 pip install PyYAML
 pip install -U Jinja2
+
+Usage:
+
+./generate_calculator.py
+
+The above command would generate the "iacs_star_calculator.html" in the html folder based on the default config
+"default.yaml". Changing the config file is possible using parameter "config". Asuming you have created a config called
+pharma.yaml this could be done using the following command.
+
+./generate_calculator.py --config pharma.yaml
 
 """
 
 
 import argparse
+import sys
+
 import yaml
 from jinja2 import Environment, FileSystemLoader
+
+
+def is_average(v1:float, v2:float, v3:float, v4:float, expected:float = 1):
+    result = (v1 + v2 + v3 +v4) / 4
+    if result == expected:
+        return True
+    return False
 
 
 def main(config_file:str, template_file:str):
@@ -27,16 +52,29 @@ def main(config_file:str, template_file:str):
 
             print(f"[*] Using template file {template_file}")
 
+            tif_average = is_average(config["risk_weight"]["TIF_LC"],config["risk_weight"]["TIF_LI"],
+                                     config["risk_weight"]["TIF_LA"], config["risk_weight"]["TIF_LAC"])
+            if not tif_average:
+                print(f"[!] TIF risk weights must have an average of 1.")
+                sys.exit()
+
+            sif_average = is_average(config["risk_weight"]["SIF_ED"], config["risk_weight"]["SIF_PD"],
+                                     config["risk_weight"]["SIF_SE"], config["risk_weight"]["SIF_R"])
+            if not sif_average:
+                print(f"[!] TIF risk weights must have an average of 1.")
+                sys.exit()
+
             context = {
-                "option_strings": config["option_strings"]
+                "option_strings": config["option_strings"],
+                "risk_weight": config["risk_weight"]
             }
 
             with open(outfile, mode="w", encoding="utf-8") as results:
                 results.write(template.render(context))
-                print(f"[!] Wrote html file {outfile}")
+                print(f"[*] Wrote html file {outfile}")
+
         except yaml.YAMLError as exc:
             print(exc)
-
 
 
 
